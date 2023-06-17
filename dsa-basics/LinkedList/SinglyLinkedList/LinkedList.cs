4f0775cb64bc25b808998e1b045892ed9551d675
Abhinandan Jaisawal <v-ajaisawal@microsoft.com>
2023-06-17 13:43:34 +0530

    Code structuring and gitignore

---
+++ dsa-basics/LinkedList/SinglyLinkedList/LinkedList.cs
@@ -0,0 +1,7 @@
+﻿namespace LinkedList.SinglyLinkedList
+{
+    internal class LinkedList
+    {
+
+    }
+}
---
+++ dsa-basics/LinkedList/SinglyLinkedList/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkedList.SinglyLinkedList
+{
+    internal class Node
+    {
+        public int key;
+        public Node link;
+        public Node(int key)
+        {
+            this.key = key;
+            link = null;
+        }
+    }
+}
